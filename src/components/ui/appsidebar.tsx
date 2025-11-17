c9dd94e909cca69e067167139a54c0e632249f02
Dev <devharwani.work@gmail.com>
2025-11-17 20:49:46 +0000

    add components for sidebar

---
+++ src/components/ui/appsidebar.tsx
@@ -0,0 +1,212 @@
+"use client"
+
+import {
+    AudioWaveform,
+    BadgeCheck,
+    Bell,
+    BookOpen,
+    Bot,
+    ChevronRight,
+    ChevronsUpDown,
+    Command,
+    CreditCard,
+    Folder,
+    Forward,
+    Frame,
+    GalleryVerticalEnd,
+    LogOut,
+    Map,
+    MoreHorizontal,
+    PieChart,
+    Plus,
+    Settings2,
+    Sparkles,
+    SquareTerminal,
+    Trash2,
+    Workflow
+} from "lucide-react"
+
+import * as React from "react"
+
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar"
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "~/components/ui/collapsible"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuAction,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubButton,
+    SidebarMenuSubItem,
+    SidebarRail,
+    useSidebar,
+} from "~/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+    user: {
+        name: "shadcn",
+        email: "m@example.com",
+        avatar: "/avatars/shadcn.jpg",
+    },
+    navMain: [
+        {
+            title: "Workflows",
+            url: "#",
+            icon: SquareTerminal,
+            isActive: true,
+        },
+        {
+            title: "Executions",
+            url: "#",
+            icon: Bot,
+        },
+        {
+            title: "Credentials",
+            url: "#",
+            icon: BookOpen,
+        },
+    ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    const { isMobile, state } = useSidebar();
+
+    return (
+        <Sidebar collapsible="icon" {...props}>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <div className="flex items-center gap-5">
+                            <Workflow />
+                            {state === "expanded" && (
+                                <span className="font-bold text-center text-black">m8m - Workflow Automation</span>
+                            )}
+                        </div>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarMenu>
+                        {data.navMain.map((item) => (
+                                <SidebarMenuItem>
+                                        <SidebarMenuButton
+                                            className={item.isActive
+                                                ? "bg-blue-600 text-white border-2 border-black rounded-md"
+                                                : ""}
+                                            tooltip={item.title}
+                                        >
+                                            {item.icon && <item.icon />}
+                                            <span>{item.title}</span>
+                                        </SidebarMenuButton>
+                                </SidebarMenuItem>
+                        ))}
+                    </SidebarMenu>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuButton
+                                    className="group-data-[state=collapsed]:hover:outline-0 group-data-[state=collapsed]:hover:bg-transparent overflow-visible"
+                                    size="lg"
+                                >
+                                    <Avatar className="h-8 w-8">
+                                        <AvatarImage
+                                            src="https://github.com/shadcn.png?size=40"
+                                            alt="CN"
+                                        />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                    <div className="grid flex-1 text-left text-sm leading-tight">
+                                        <span className="truncate font-heading">
+                                            {data.user.name}
+                                        </span>
+                                        <span className="truncate text-xs">{data.user.email}</span>
+                                    </div>
+                                    <ChevronsUpDown className="ml-auto size-4" />
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent
+                                className="w-[--radix-dropdown-menu-trigger-width] min-w-56"
+                                side={isMobile ? "bottom" : "right"}
+                                align="end"
+                                sideOffset={4}
+                            >
+                                <DropdownMenuLabel className="p-0 font-base">
+                                    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                        <Avatar className="h-8 w-8">
+                                            <AvatarImage
+                                                src="https://github.com/shadcn.png?size=40"
+                                                alt="CN"
+                                            />
+                                            <AvatarFallback>CN</AvatarFallback>
+                                        </Avatar>
+                                        <div className="grid flex-1 text-left text-sm leading-tight">
+                                            <span className="truncate font-heading">
+                                                {data.user.name}
+                                            </span>
+                                            <span className="truncate text-xs">
+                                                {data.user.email}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuGroup>
+                                    <DropdownMenuItem>
+                                        <Sparkles />
+                                        Upgrade to Pro
+                                    </DropdownMenuItem>
+                                </DropdownMenuGroup>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuGroup>
+                                    <DropdownMenuItem>
+                                        <BadgeCheck />
+                                        Account
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        <CreditCard />
+                                        Billing
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>
+                                        <Bell />
+                                        Notifications
+                                    </DropdownMenuItem>
+                                </DropdownMenuGroup>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                    <LogOut />
+                                    Log out
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
+            <SidebarRail />
+        </Sidebar>
+    )
+}
