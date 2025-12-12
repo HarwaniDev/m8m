1a032cf9e1b199f76863be8104eaa26ef8bd4e12
Dev <devharwani.work@gmail.com>
2025-12-12 05:57:25 +0000

    complete the http execution step

--- src/components/executions/http-request/executor.ts
+++ src/components/executions/http-request/executor.ts
@@ -1,6 +1,12 @@
+import { NonRetriableError } from "inngest";
 import type { NodeExecutor } from "~/components/triggers/types";
+import axios from "axios";
 
-type httpRequestData = Record<string, unknown>;
+type httpRequestData = {
+    endpoint?: string;
+    method?: "GET" | "POST" | "PATCH" | "PUT" | "DELETE";
+    body?: string;
+};
 
 export const httpRequestExecutor: NodeExecutor<httpRequestData> = async ({
     data,
@@ -8,6 +14,31 @@ export const httpRequestExecutor: NodeExecutor<httpRequestData> = async ({
     context,
     step
 }) => {
-    const result = await step.run("http-request", async () => context);
+    if (!data.endpoint) {
+        throw new NonRetriableError("HTTP request node: no endpoint configured");
+    }
+    const result = await step.run("http-request", async () => {
+        const endpoint = data.endpoint;
+        const method = data.method || "GET";
+        let body;
+        if (["POST", "PUT", "PATCH"].includes(method)) {
+            body = data.body;
+        }
+
+        const response = await axios.request({
+            url: endpoint,
+            method: method,
+            data: body
+        });
+
+        return {
+            ...context,
+            httpResponse: {
+                status: response.status,
+                statusText: response.statusText,
+                data: response.data
+            }
+        }
+    })
     return result;
 };
