f78d9557e0b733d87e57ca863e3fcc422118faf9
Dev <devharwani.work@gmail.com>
2025-12-14 09:21:47 +0000

    add google form trigger node in node-component.tsx

--- src/config/node-component.ts
+++ src/config/node-component.ts
@@ -2,12 +2,14 @@ import type { NodeTypes } from "@xyflow/react";
 import { NodeType } from "generated/prisma";
 import { HTTPRequestNode } from "~/components/executions/http-request/node";
 import { InitialNode } from "~/components/initial/node";
+import GoogleFormTriggerNode from "~/components/triggers/google-form-trigger/node";
 import ManualTriggerNode from "~/components/triggers/manual-trigger/node";
 
 export const nodeComponents = {
     [NodeType.INITIAL]: InitialNode,
     [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
-    [NodeType.HTTP_REQUEST]: HTTPRequestNode
+    [NodeType.HTTP_REQUEST]: HTTPRequestNode,
+    [NodeType.GOOGLE_FORM_TRIGGER]: GoogleFormTriggerNode
 } as const satisfies NodeTypes
 
 export type RegisteredNodeType = keyof typeof nodeComponents;
