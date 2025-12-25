e60a6f25eb40ac077ec79fa7a0b0f86cdefb4010
Dev <devharwani.work@gmail.com>
2025-12-25 15:28:54 +0000

    update executor registry

--- src/lib/executor-registry.ts
+++ src/lib/executor-registry.ts
@@ -1,6 +1,8 @@
 import { NodeType } from "generated/prisma";
+import { DiscordExecutor } from "~/components/executions/discord/executor";
 import { GeminiExecutor } from "~/components/executions/gemini/executor";
 import { httpRequestExecutor } from "~/components/executions/http-request/executor";
+import { TelegramExecutor } from "~/components/executions/telegram/executor";
 import type { NodeExecutor } from "~/components/executions/types";
 import { GoogleFormTriggerExecutor } from "~/components/triggers/google-form-trigger/executor";
 import { ManualTriggerExecutor } from "~/components/triggers/manual-trigger/executor";
@@ -10,7 +12,10 @@ export const executorRegistry: Record<NodeType, NodeExecutor> = {
     [NodeType.MANUAL_TRIGGER]: ManualTriggerExecutor,
     [NodeType.HTTP_REQUEST]: httpRequestExecutor,
     [NodeType.GOOGLE_FORM_TRIGGER]: GoogleFormTriggerExecutor,
-    [NodeType.GEMINI]: GeminiExecutor
+    [NodeType.GEMINI]: GeminiExecutor,
+    [NodeType.DISCORD]: DiscordExecutor,
+    [NodeType.TELEGRAM]: TelegramExecutor
+
 }
 
 export const getExecutor = (nodeType: NodeType): NodeExecutor => {
