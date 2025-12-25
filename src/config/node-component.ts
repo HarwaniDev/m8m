ad5f6302ad6f0b87bbb375ec2128e3648707379d
Dev <devharwani.work@gmail.com>
2025-12-25 15:28:04 +0000

    add discord and telegram to node components

--- src/config/node-component.ts
+++ src/config/node-component.ts
@@ -1,7 +1,9 @@
 import type { NodeTypes } from "@xyflow/react";
 import { NodeType } from "generated/prisma";
+import { DiscordNode } from "~/components/executions/discord/node";
 import { GeminiNode } from "~/components/executions/gemini/node";
 import { HTTPRequestNode } from "~/components/executions/http-request/node";
+import { TelegramNode } from "~/components/executions/telegram/node";
 import { InitialNode } from "~/components/initial/node";
 import GoogleFormTriggerNode from "~/components/triggers/google-form-trigger/node";
 import ManualTriggerNode from "~/components/triggers/manual-trigger/node";
@@ -11,7 +13,9 @@ export const nodeComponents = {
     [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
     [NodeType.HTTP_REQUEST]: HTTPRequestNode,
     [NodeType.GOOGLE_FORM_TRIGGER]: GoogleFormTriggerNode,
-    [NodeType.GEMINI]: GeminiNode
+    [NodeType.GEMINI]: GeminiNode,
+    [NodeType.DISCORD]: DiscordNode,
+    [NodeType.TELEGRAM]: TelegramNode
 } as const satisfies NodeTypes
 
 export type RegisteredNodeType = keyof typeof nodeComponents;
